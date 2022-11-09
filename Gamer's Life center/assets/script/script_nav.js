7068fc1a49804178ff1afa35e9c7cbe587b93b31
Ver3Parfait <militellolucas@icloud.com>
2022-11-09 12:20:39 +0100

    3.15
    Ajout du responsive pour le footer

--- Gamer's Life center/assets/script/script_nav.js
+++ Gamer's Life center/assets/script/script_nav.js
@@ -1,7 +1,15 @@
 
     const menuHamburger = document.querySelector(".menu_btn")
     const navLinks = document.querySelector("#nav_links")
+    const body = document.querySelector('body')
 
     menuHamburger.addEventListener('click',()=>{
+    
     navLinks.classList.toggle('mobile-menu')
+    if (navLinks.classList.contains('mobile-menu')) {
+        body.style.overflow = "hidden"
+    }else{
+        body.style.overflow = "scroll"
+  
+    }
     })
